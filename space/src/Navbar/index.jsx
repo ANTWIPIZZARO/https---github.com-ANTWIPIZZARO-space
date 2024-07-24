a7b07684ab1280b3d6c7fd2cbf27002dfe0cf2cc
clinton <pizzarogh003@gmail.com>
2024-07-24 05:07:18 -0700

    first commit

---
+++ space/src/Navbar/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+  return;
+  <div className="">
+    <ul className="text-blue-900">
+      <li>Home</li>
+      <li>Pricing</li>
+      <li>About Us</li>
+      <li> Contact Us</li>
+    </ul>
+  </div>;
+};
